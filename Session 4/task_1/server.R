e2d9bb94c116293e7415fbc499544e1702de017b
Fran <francesca.bryden@dft.gov.uk>
2022-07-06 13:31:48 +0100

    Added session 4 code

---
+++ Session 4/task_1/server.R
@@ -0,0 +1,71 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(gapminder)
+library(dplyr)
+
+eurovision_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-17/eurovision.csv')
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$new_chart <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+  eurovision_filtered <- reactive({
+    
+    eurovision_data %>%
+      dplyr::filter(year == input$year)
+    
+  })
+  
+  output$eurovision_chart <- renderPlot({
+   
+    ggplot(eurovision_filtered(), aes(x = artist_country, y = rank))+
+      geom_bar(stat = "identity")
+    
+  })
+  
+  ##Create a text output
+  
+  output$date_text <- renderText({
+    
+    paste("Welcome to the app! Today's date is: ", lubridate::today())
+    
+  })
+  
+
+  
+  ##Create a data table output
+  
+  output$eurovision <- renderDataTable({
+    
+    eurovision_filtered()
+    
+  })
+  
+  output$download_data <- downloadHandler(
+      filename = function() {
+        paste('data-', Sys.Date(), '.csv', sep='')
+      },
+      content = function(con) {
+        write.csv(eurovision_filtered(), con)
+      }
+    )
+  
+}) #End of server
